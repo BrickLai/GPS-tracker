9475ce5ac8e2efedc5317f6f89d78c7611ad95da
Brick Lai <l279810259@gmail.com>
2022-12-13 10:43:15 -0600

    second

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,17 +1,77 @@
 /* ----------------------------------------------
 
-    Object-oriented JavaScript
+    Intro to Third-Party APIs
     Zhaoyu Li
-
-    Optional chainning operator
-
-    The optional chaining operator is like the dot chaining operator (.), except
- that instead of causing an error if a reference is nullish (null or
- undefined), the expression short-circuits with a return value of undefined.
- When used with function calls, it returns undefined if the given function
- does not exist
 
 -----------------------------------------------*/
 
+
 'use strict';
-function select (selector, paarent = document)
+function select (selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+const overlay = select ('.overlay');
+const loading = select ('.loading');
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpY2s5NzMiLCJhIjoiY2xiZ3J1czJ1MGVlMjNubzN3M2cxeDB0byJ9.Wk6nus9qpX7R3l9G8TXcnQ';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    interactive: false,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    pitch: 40,
+    zoom: 16
+});
+
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker ({
+    color: '#3898ff'
+})
+
+function getLocation(position) {
+    const {longitude, latitude} = position.coords;
+
+    if (longitude && latitude) {
+        map.setCenter([longitude,latitude]);
+        marker.setLngLat([longitude,latitude]).addTo(map);
+        setTimeout(()=>{ overlay.classList.add('hidden')}, 750);
+    }
+
+}
+
+function errorHandler(event) {
+    loading.style.animationPlayState = 'paused';
+    console.log(event.message);
+}
+
+const options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+};
+
+/*
+    The watchPosition() method is used to register a handler function that will be called automatically each time the position of the device changes
+*/
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(getLocation, errorHandler,options);
+}else{
+    console.log('Geolocation is not supported by your browser');
+}
+
+
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpY2s5NzMiLCJhIjoiY2xiZ3J1czJ1MGVlMjNubzN3M2cxeDB0byJ9.Wk6nus9qpX7R3l9G8TXcnQ';
+// const map = new mapboxgl.Map({
+//      container: 'map', // container ID
+//      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+//      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//      center: [-74.5, 40], // starting position [lng, lat]
+//      zoom: 9 // starting zoom
+// });
